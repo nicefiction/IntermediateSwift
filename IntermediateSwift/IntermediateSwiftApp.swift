ab66f1facb01d5bb912bac056d6c0bbb4841d6c9
Olivier Van hamme <nicefiction@gmail.com>
2020-12-22 11:11:05 +0100

    Initial Commit

---
+++ IntermediateSwift/IntermediateSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateSwiftApp.swift
+//  IntermediateSwift
+//
+//  Created by Olivier Van hamme on 22/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
